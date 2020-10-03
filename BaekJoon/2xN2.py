b335daddc6032bd8b343f3c7dd3d0ef1cf03bb49
Nam-Ki-Bok <nkb7714@naver.com>
2020-10-03 16:26:47 +0900

    [2020.10.03]

--- BaekJoon/2xN2.py
+++ BaekJoon/2xN2.py
@@ -1,19 +1,21 @@
 from math import factorial
 
 
-def cal(n, r):
+def cal(n, r, k):
 
-	return factorial(n) // (factorial(n - r) * factorial(r))
+	return (factorial(n) // (factorial(n - r) * factorial(r))) * k
 
 
 answer = 0
 weight = int(input())
 n = weight
 r = 0
+k = 1
 
 while n >= r:
-	answer += cal(n, r)
+	answer += cal(n, r, k)
 	n -= 1
 	r += 1
+	k *= 2
 
 print(answer % 10007)
